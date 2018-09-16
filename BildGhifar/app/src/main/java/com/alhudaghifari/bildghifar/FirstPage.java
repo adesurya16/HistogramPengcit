188b6f9bae95e1cff7ea8a93b0d5f07ca0db2ebe
alhudaghifari <alhudaghifari@gmail.com>
2018-09-16 22:07:13 +0700

    button to tugas4 added

--- BildGhifar/app/src/main/java/com/alhudaghifari/bildghifar/FirstPage.java
+++ BildGhifar/app/src/main/java/com/alhudaghifari/bildghifar/FirstPage.java
@@ -8,6 +8,7 @@ import android.view.View;
 import com.alhudaghifari.bildghifar.tugas1Histogram.LandingPageHistogram;
 import com.alhudaghifari.bildghifar.tugas2carigarismuka.LandingPageTugas2;
 import com.alhudaghifari.bildghifar.tugas3Equalizer.LandingPageTugas3Equalizer;
+import com.alhudaghifari.bildghifar.tugas4ChainCode.LandingPageTugas4;
 
 public class FirstPage extends AppCompatActivity {
 
@@ -29,6 +30,11 @@ public class FirstPage extends AppCompatActivity {
 
     public void gotoTugas3(View view) {
         Intent intent = new Intent(FirstPage.this, LandingPageTugas3Equalizer.class);
+        startActivity(intent);
+    }
+
+    public void gotoTugas4(View view) {
+        Intent intent = new Intent(FirstPage.this, LandingPageTugas4.class);
         startActivity(intent);
     }
 }
